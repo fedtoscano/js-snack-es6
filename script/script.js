90cda96b0b1f238f81f01ed4b2e6b3c2a514ff5c
fedtoscano <federicotoscano611@gmail.com>
2024-05-23 13:15:04 +0200

    snack 1

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const guests = [
+    {tableName:"Tavolo Vip",guestName:"Brad Pitt",place:1},
+    {tableName:"Tavolo Vip",guestName:"Johnny Depp",place:2},
+    {tableName:"Tavolo Vip",guestName:"Lady Gaga",place:3},
+    {tableName:"Tavolo Vip",guestName:"Cristiano Ronaldo",place:4},
+    {tableName:"Tavolo Vip",guestName:"Georgina Rodriguez",place:5},
+    {tableName:"Tavolo Vip",guestName:"Chiara Ferragni",place:6},
+    {tableName:"Tavolo Vip",guestName:"George Clooney",place:7},
+    {tableName:"Tavolo Vip",guestName:"Amal Clooney",place:8},
+    {tableName:"Tavolo Vip",guestName:"Fedez",place:9},
+    {tableName:"Tavolo Vip",guestName:"Amadeus",place:10},
+    {tableName:"Tavolo Vip",guestName:"Fiorello",place:11},
+]
+
+const typographyList = guests.map((guest)=>{
+    console.log(`${guest.guestName} - ${guest.tableName} at place ${guest.place}`)
+})
+
