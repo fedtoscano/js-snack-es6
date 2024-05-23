8451bbffabefb58ff893d4fdf311d58e44c2a649
fedtoscano <federicotoscano611@gmail.com>
2024-05-23 13:19:41 +0200

    snack 2

--- script/script.js
+++ script/script.js
@@ -16,3 +16,30 @@ const typographyList = guests.map((guest)=>{
     console.log(`${guest.guestName} - ${guest.tableName} at place ${guest.place}`)
 })
 
+const students = [
+    { Id: 213, Name: "Giuseppina della Rovere", Grades: 78 },
+    { Id: 110, Name: "Paola Cortellessa", Grades: 96 },
+    { Id: 250, Name: "Andrea Mantegna", Grades: 48 },
+    { Id: 145, Name: "Gaia Borromini", Grades: 74 },
+    { Id: 196, Name: "Luigi Grimaldello", Grades: 68 },
+    { Id: 102, Name: "Piero della Francesca", Grades: 50 },
+    { Id: 120, Name: "Francesca da Polenta", Grades: 84 }
+];
+
+const uppercaseList = students.map((student) =>{
+    console.log(student.Name.toUpperCase())
+})
+
+const gradesOver70 = students.filter((student) =>{
+    if(student.Grades>70)
+        return true
+})
+
+console.log(gradesOver70)
+
+const gradesAndID = students.filter((student) =>{
+    if(student.Grades>70 && student.Id>120)
+        return true
+})
+
+console.log(gradesAndID)
